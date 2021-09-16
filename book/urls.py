10d1b26faddf8937fb246feea306bfae07d73336
ssangmmin <singthesong419@gmail.com>
2021-09-16 16:29:49 +0900

    9/14 예제 숙제중

--- book/urls.py
+++ book/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.book_detail), #FBV 방식
-    path('', views.book_list), #FBV 방식
+    path('<int:pk>/', views.book_detail.as_view()), #FBV 방식
+    path('',views.book_list.as_view()),
+    # path('', views.book_list), #FBV 방식
 ]
--- book/views.py
+++ book/views.py
@@ -1,26 +1,35 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from .models import Book
-
-def book_list(request):
-    books = Book.objects.all().order_by('-pk')
-
-    return render(
-        request,
-        'book/book_list.html',
-        {
-            'books': books, #왼쪽은 templates에서 사용할 변수명
-        }
-    )
-
-def book_detail(request, pk):
-    book = Book.objects.get(pk=pk)
-
-    return render(
-        request,
-        'book/book_detail.html',
-        {
-            'b': book,
-        }
-    )
+from django.views.generic import ListView, DetailView
 
 
+class book_list(ListView):
+    model = Book
+    ordering = '-pk'
+
+
+# def book_list(request):
+#     books = Book.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'book/book_list.html',
+#         {
+#             'books': books, #왼쪽은 templates에서 사용할 변수명
+#         }
+#     )
+
+class book_detail(DetailView):
+    model = Book
+
+# def book_detail(request, pk):
+#     book = Book.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'book/book_detail.html',
+#         {
+#             'b': book,
+#         }
+#     )
+#
