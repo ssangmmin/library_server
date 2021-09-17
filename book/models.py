7691b8825b1f04dbc10a6bef7ad2256dd7494a7d
ssangmmin <singthesong419@gmail.com>
2021-09-17 14:20:42 +0900

    BOOK 모델에 file_upload 필드 추가하기

--- book/models.py
+++ book/models.py
@@ -10,7 +10,9 @@ class Book(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
+
     head_image = models.ImageField(upload_to='book/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='book/files/%Y/%m/%d/', blank=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
