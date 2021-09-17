6319470b56644a11e8289083567ba0b510b80d49
ssangmmin <singthesong419@gmail.com>
2021-09-17 15:56:27 +0900

    첨부 파일이 있을 경우 책 상세 페이지에 다운로드 버튼 출력하기

--- book/models.py
+++ book/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 
 class Book(models.Model):
@@ -19,4 +20,10 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return f'/book/{self.pk}'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
 
