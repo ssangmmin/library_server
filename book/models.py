785ae876e0d4a3c1dc76daa7d6f6529948df276b
ssangmmin <singthesong419@gmail.com>
2021-09-23 16:17:10 +0900

    author 필드 추가하기 p.295

--- book/models.py
+++ book/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 
@@ -13,11 +14,13 @@ class Book(models.Model):
     content = models.CharField(max_length=1500)
     hook_text = models.CharField(max_length=100, blank=True)
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     head_image = models.ImageField(upload_to='book/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='book/files/%Y/%m/%d/', blank=True)
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/book/{self.pk}'
