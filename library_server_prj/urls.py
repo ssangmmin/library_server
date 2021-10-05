6871f9901687de8db9d1172ed21496a9cb3ddb6a
ssangmmin <singthesong419@gmail.com>
2021-10-05 16:53:12 +0900

    구글 로그인 기능 추가하기 p.457

--- library_server_prj/urls.py
+++ library_server_prj/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('accounts/', include('allauth.urls')),
     path('markdownx/', include('markdownx.urls')),
     path('book/', include('book.urls')),
     path('admin/', admin.site.urls),
